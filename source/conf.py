6bab0ecb60d7659b34f181ac967f91ba24cf2d2f
Maxime Delorme <maxime.delorme@gmail.com>
2025-10-07 09:50:46 +0200

    Math extension

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = 'The Dyablo Team'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.imgmath']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -25,3 +25,6 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Sphinx math configuration
+imgmath_image_format = 'svg'
