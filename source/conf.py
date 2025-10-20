4f7c9180c1e6d772a09d35d80c1902dccfe4aa7d
Maxime Delorme <maxime.delorme@gmail.com>
2025-10-20 11:54:13 +0200

    Relevant doxygen files

--- source/conf.py
+++ source/conf.py
@@ -1,3 +1,5 @@
+import os
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -13,7 +15,7 @@ author = 'The Dyablo Team'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.imgmath']
+extensions = ['sphinx.ext.imgmath', 'breathe']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -25,6 +27,20 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
 
 # -- Sphinx math configuration
 imgmath_image_format = 'svg'
+imgmath_font_size = 16
+
+
+# -- Breathe - Doxygen/Sphinx connection -------------------------------------
+## Only include Breathe if the doxygen path has been imported into the project
+if os.path.exists('../doxygen'):
+    extensions.append('breathe')
+    breathe_projects = {
+        "reference": "../doxygen"
+    }
+    breathe_default_project = "reference"
